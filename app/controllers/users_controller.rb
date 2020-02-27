0772ef887e3743bfd68f2f0cff79e55ac228bae5
Robin Weidlich <robinweidlich7@gmail.com>
2020-02-27 12:54:55 -0300

    some controller work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		@user.save
+
+		redirect_to user_path(@user)
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :role, :age, :styles, :description)
+	end
 end
