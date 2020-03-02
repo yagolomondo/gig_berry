f6f31c222ac10e5c4c40d9dfc5ba1ae1dc17bf6d
yago santos <yagolcs@msn.com>
2020-03-02 17:24:36 -0300

    Added and implemented cloudinary gem adding photos to concert_halls

--- app/models/concert_hall.rb
+++ app/models/concert_hall.rb
@@ -1,6 +1,7 @@
 class ConcertHall < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
+  has_many_attached :photos
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
   include PgSearch::Model
