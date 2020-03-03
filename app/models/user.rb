61961de954e132d7eae65dd0ff7684d3c70b5eb6
yago santos <yagolcs@msn.com>
2020-03-02 21:56:13 -0300

    Added photo to users and implemented avatar display on navbar. Also set a default photo.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :concert_halls
   has_many :reservations
+  has_one_attached :photo
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
