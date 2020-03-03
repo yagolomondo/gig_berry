618615d277687cdb0c1601408fd532b42ae694a6
breno romeiro <bsromeiro@gmail.com>
2020-03-03 15:19:16 -0300

    re-edited create concert-hall

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,8 @@ class RegistrationsController < Devise::RegistrationsController
     def after_update_path_for(user)
       user_path(user)
     end
+
+    # def after_destroy_path_for(user)
+    #   concert_halls_path
+    # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :concert_halls
-  has_many :reservations
+  has_many :concert_halls, dependent: :destroy
+  has_many :reservations, dependent: :destroy
   has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
